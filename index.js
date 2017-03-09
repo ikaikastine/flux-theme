23449834720e7c95435e54a19bbcaebcf81ca179
Kevin Stine <ikaikastine@gmail.com>
2017-03-09 13:53:58 -0800

    fixed colors to work properly

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const blue = '#90d8ff'
 const cyan = '#90ffef'
 const green = '#50fa7b'
 const orange = '#fad050'
+const magenta = '#bd93f9'
 const red = '#fa7b50'
 const yellow = '#fad050'
 const white = '#ffffff'
@@ -18,27 +19,24 @@ const lightMagenta = '#d0b3fa'
 const lightCyan = '#90ffef'
 const lightWhite = '#ffffff'
 
+
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
-    borderColor: '#333',
-    cursorColor: 'rgba(238, 238, 238, 0.8)',
-    foregroundColor,
     backgroundColor,
+    foregroundColor,
     colors: [
       black,
-      blue,
-      cyan,
-      green,
-      orange,
-      magenta,
       red,
+      green,
       yellow,
+      blue,
+      magenta,
+      cyan,
       white,
       lightBlack,
       lightRed,
       lightGreen,
       lightYellow,
-      lightOrange,
       lightBlue,
       lightMagenta,
       lightCyan,
